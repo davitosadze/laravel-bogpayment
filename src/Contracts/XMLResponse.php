c1c6ae55645b6aabef61bfdc11a480296c536964
davitosadze <39856965+davitosadze@users.noreply.github.com>
2023-08-23 14:31:33 +0400

    card on file

--- src/Contracts/XMLResponse.php
+++ src/Contracts/XMLResponse.php
@@ -51,6 +51,12 @@ class XMLResponse
             <exponent>2</exponent>
         </account-amount>
     </purchase>
+       <order-params>
+        <param>
+            <name>card_on_file</name>
+            <value>CIT</value>
+        </param>
+    </order-params>
 </payment-avail-response>
 XML;
 
